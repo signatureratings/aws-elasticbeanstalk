fab61e7748d1410ea9681e15bc225601fb7594a2
sairam balu <balusairam26@gmail.com>
2024-03-05 16:19:56 -0500

    testing

--- scripts/deploy2.sh
+++ scripts/deploy2.sh
@@ -7,30 +7,16 @@ REGION="us-east-1"
 BUCKET_NAME="deploymentfilesbucket"
 BUILD_FILE="build.zip"
 
-# Create Elastic Beanstalk application
-aws elasticbeanstalk create-application --application-name $APP_NAME --region $REGION
 
 
-aws s3api create-bucket --bucket $BUCKET_NAME --region $REGION --create-bucket-configuration LocationConstraint=$REGION
+# # Set bucket policy
 
-# Set bucket policy
-aws s3api put-bucket-policy --bucket $BUCKET_NAME --policy '{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "AllowAWSServices",
-            "Effect": "Allow",
-            "Principal": {
-                "Service": "elasticbeanstalk.amazonaws.com"
-            },
-            "Action": "s3:GetObject",
-            "Resource": "arn:aws:s3:::'$BUCKET_NAME'/*"
-        }
-    ]
-}'
 
 # Upload build file to S3
-aws s3 cp $BUILD_FILE s3://$BUCKET_NAME/$BUILD_FILE --region $REGION
+aws s3 cp ../$BUILD_FILE s3://$BUCKET_NAME/$BUILD_FILE || echo "An error occurred while copying the file to s3 bucket: $?" >&2
+
+# Create Elastic Beanstalk application
+aws elasticbeanstalk create-application --application-name $APP_NAME --region $REGION || echo "An error occurred while creating beanstalk app: $?" >&2
 
 # Create Elastic Beanstalk environment and deploy build file
-aws elasticbeanstalk create-environment --application-name $APP_NAME --environment-name $ENV_NAME --version-label $BUILD_FILE --solution-stack-name "64bit Amazon Linux 2023 v6.1.1 running Node.js 20" --region $REGION
+aws elasticbeanstalk create-environment --application-name $APP_NAME --environment-name $ENV_NAME --version-label $BUILD_FILE --solution-stack-name "64bit Amazon Linux 2023 v6.1.1 running Node.js 20" --region $REGION || echo "An error occurred while creating the beanstalk environment: $?" >&2
